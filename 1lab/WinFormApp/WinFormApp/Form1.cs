f47903f4fe94e8876dca5b589f6dbcd2f1bbbecd
Nikita Kalinichev <nikitakalinichev588@gmail.com>
2024-02-27 17:28:30 +0700

    1-4 labs

---
+++ 1lab/WinFormApp/WinFormApp/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace WinFormApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+        private void edAngle_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        const double C = 0.15;
+        const double rho = 1.29;
+        const double g = 9.81;
+        double height, angle, speed, S, m, dt;
+        double cosa, sina, beta, k;
+
+        double t, x, y, vx, vy;
+        double x_max, x_old, y_max, y_old;
+
+        private void btStart_Click(object sender, EventArgs e)
+        {
+            dt = (double)edStep.Value;
+            height = (double)edHeight.Value;
+            speed = (double)edSpeed.Value;
+            angle = (double)edAngle.Value;
+            S = (double)edSize.Value;
+            m = (double)edWeight.Value;
+            x_max = 0;
+            y_max = 0;
+
+            cosa = Math.Cos(angle * Math.PI / 180);
+            sina = Math.Sin(angle * Math.PI / 180);
+
+            beta = 0.5 * C * S * rho;
+            k = beta / m;
+
+            t = 0; 
+            x = 0; 
+            y = height;
+            vx = speed * cosa;
+            vy = speed * sina;
+
+            //chart1.Series[0].Points.Clear();
+            chart1.Series[0].Points.AddXY(x, y);
+
+            timer1.Start();
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            double vx_old = vx;
+            double vy_old = vy;
+            double root = Math.Sqrt(vx * vx + vy * vy);
+            t = t + dt;
+
+            vx = vx_old - k * vx_old * root * dt;
+            vy = vy_old - (g + k * vy_old * root) * dt;
+
+            x = x + vx * dt;
+            y = y + vy * dt;
+
+            if (x > x_max) x_max = x;
+            if (y > y_max) y_max = y;
+
+            chart1.Series[0].Points.AddXY(x, y);
+
+            if (y <= 0)
+            {
+                edMaxX.Value = (decimal)(x_old + (y_old * (x_max - x_old) / (y_old - y)));
+                edMaxY.Value = (decimal)y_max;
+                edEndV.Value = (decimal)(Math.Sqrt(vx * vx + vy * vy));
+                
+                timer1.Stop();
+            }
+            y_old = y;
+            x_old = x_max;
+        }
+
+    }
+}
---
+++ 3lab/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using System.Xml.Schema;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        int cells = 2;
+        int[,] colorsArray;
+        int rows = 1;
+        bool flag = true;
+        public Form1()
+        {
+            InitializeComponent();
+            Init();
+        }
+        void Init()
+        {
+            ClientSize = new Size(750, 600);
+            MouseDown += paint;
+            colorsArray = new int[(ClientSize.Width - 150) / cells, ClientSize.Height / cells];
+            DoubleBuffered = true;
+
+        }
+        private void paint(object sender, MouseEventArgs e)
+        {
+            var thisX = e.X;
+            colorsArray[0, thisX / cells] = colorsArray[0, thisX / cells] == 1 ? 0 : 1;
+            Invalidate();
+        }
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            InitCells(e.Graphics);
+            if(cells > 2) InitGrid(e.Graphics);
+        }
+        protected void InitGrid(Graphics g)
+        {
+            for (int i = 0; i <= rows; i++)
+            {
+                g.DrawLine(new Pen(Color.Black, 1), new Point(0, cells * i), new Point(ClientSize.Width - 150, cells * i));
+            }
+            for (int i = 0; i <= (ClientSize.Width - 150) / cells; i++)
+            {
+                g.DrawLine(new Pen(Color.Black, 1), new Point(cells * i, 0), new Point(cells * i, cells * rows));
+            }
+        }
+        protected void InitCells(Graphics g)
+        {
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < (ClientSize.Width - 150) / cells; j++)
+                {
+                    if (colorsArray[i, j] == 1)
+                    {
+                        g.FillRectangle(Brushes.Black, cells * j, cells * i, cells, cells);
+                    }
+                    else
+                    {
+                        g.FillRectangle(Brushes.White, cells * j, cells * i, cells, cells);
+                    }
+                }
+            }
+        }
+        private void Clear()
+        {
+            colorsArray = new int[(ClientSize.Width - 150) / cells, ClientSize.Height / cells];
+            rows = 1;
+            Invalidate();
+        }
+        private void Display()
+        {
+            string binvalue = Convert.ToString((int)rule.Value, 2);
+            if (binvalue.Length < 8)
+            {
+                binvalue = binvalue.PadLeft(8, '0'); 
+            }
+            
+            rows = ClientSize.Height / cells;
+            for (int i = 1; i < rows; i++)
+            {
+                for (int j = 0; j < (ClientSize.Width - 150) / cells; j++)
+                {
+                    var l = (j - 1 + (ClientSize.Width - 150) / cells) % ((ClientSize.Width - 150) / cells);
+                    var r = (j + 1) % ((ClientSize.Width - 150) / cells);
+
+                    string[] variation = new string[] { "111", "110", "101", "100", "011", "010", "001", "000" };
+
+                    for (int k = 0; k < variation.Length; k++)
+                    {
+                        if (colorsArray[i - 1, l].ToString() + colorsArray[i - 1, j].ToString() + colorsArray[i - 1, r].ToString() == variation[k])
+                        {
+                            colorsArray[i, j] = int.Parse(binvalue[k].ToString());
+                            break;
+                        }
+                    }
+                }
+            }
+            Invalidate();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (flag)
+            {
+                flag = false;
+                Display();
+                button1.Text = "CLEAR";
+            }
+            else
+            {
+                Clear();
+                flag = true;
+                button1.Text = "DISPLAY";
+            }
+        }
+    }
+}
