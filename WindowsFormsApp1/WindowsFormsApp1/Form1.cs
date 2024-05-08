d49b8129b0b3fece2e877931084f1134c193f7f2
Nikita Kalinichev <nikitakalinichev588@gmail.com>
2024-05-08 17:11:21 +0700

    up_11lab_3

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -1,93 +1,133 @@
 ﻿using System;
+using System.Linq;
 using System.Windows.Forms;
+using MathNet.Numerics.Distributions;
+using MathNet.Numerics.Statistics;
 
 namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
-        private int numOfTeam = 6;
-        private int [,] team;
+        private int numOfExperiments;
+        private double[] rundomVar;
+        private double gap;
+        private int[] counts;
+        int k = 15;
+
+        private double generalProbability = 0;
         private Random rnd = new Random();
+        double mathExp;
+        double mathVar;
+        double ksi;
+        double empMathVar;
+        double empMathExp;
+        double empMathVarError;
+        double empMathExpError;
+        double maxValue;
+        double minValue;
+
+        double mathExpUser;
+        double mathVarUser;
         public Form1()
         {
             InitializeComponent();
-            team = new int[numOfTeam, 8];
+            mathExp = 0;
+            mathVar = 0;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            //numOfExperiments = (int)numericUpDown7.Value;
-            dataGridView3.Rows.Clear();
-            resultGenerate();
+            numOfExperiments = (int)numericUpDown7.Value;
+            rundomVar = new double[numOfExperiments];
+            mathVarUser = (double)numericUpDown1.Value;
+            mathExpUser = (double)numericUpDown2.Value;
+
+            counts = new int[k];
+            chart1.Series[0].Points.Clear();
+
+            listGenerate();
+            gridGenerate();
+            chi();
             visualisation();
         }
-        private void resultGenerate()
-        {
-            for (int i = 0; i < numOfTeam; i++)
+        private void gridGenerate() {
+            maxValue = rundomVar.Max();
+            minValue = rundomVar.Min();
+            double sub = maxValue - minValue + 0.01;
+            gap = sub / k;
+            for (int i = 0; i < rundomVar.Length; i++)
             {
-                team[i, 0] = i + 1;
-                for (int j = i + 1; j < numOfTeam; j++)
+                int j = 0;
+                do
                 {
-                    if(i == j) { continue; }
-                    else
-                    {
-                        double currentRnd = rnd.NextDouble() * 10 / 2;
-                        int count_i = eval(currentRnd);
-                        int count_j = eval(currentRnd);
-                        team[i, 6] += count_i;
-                        team[i, 7] += count_j;
-                        team[j, 6] += count_j;
-                        team[j, 7] += count_i;
-                        if (count_i == count_j)
-                        {
-                            team[i, 3]++;
-                            team[j, 3]++;
-                            team[i, 5]++;
-                            team[j, 5]++;
-                        }
-                        else
-                        {
-                            if (count_i > count_j)
-                            {
-                                team[i, 2] ++;
-                                team[i, 5] += 3;
-                                team[j, 4] ++;
-                            }
-                            else
-                            {
-                                team[j, 2]++;
-                                team[j, 5] += 3;
-                                team[i, 4]++;
-                            }
-                        }
-                    }
-                }
-                team[i, 1] = team[i, 3] + team[i, 4] + team[i, 2];
+                    j++;
+                } while (minValue + j * gap <= rundomVar[i]);
+                counts[j - 1]++;
             }
         }
-        private int eval(double lambda)
+        private void listGenerate()
         {
-            int i = 0;
-            double S = 0;
-            do
+            for (int i = 0; i < numOfExperiments; i++)
             {
-                double a = rnd.NextDouble();
-                S += Math.Log(a);
-                i++;
-            } while (S >= -lambda);
-            return i - 1;
+                int n = 12;
+                double rv = 0;
+                for (int j = 0; j < n; j++)
+                {
+                    rv += rnd.NextDouble();
+                }
+                rv = rv - 6;
+                double x = eval(rv);
+                x = mathExpUser + Math.Sqrt(mathVarUser) * x;
+                rundomVar[i] = x;
+            }
+        }
+        private double eval(double rv)
+        {
+            return rv + (1 / 240.0) * (Math.Pow(rv, 3) - 3 * rv);
+        }
+        private void chi()
+        {
+            var distribution = new Normal(mathExpUser, Math.Sqrt(mathVarUser));
+            ksi = 0;
+            for (int i = 0; i < k; i++)
+            {
+                double a = minValue + i * gap;
+                double b = a + gap;
+                double prob_a = distribution.CumulativeDistribution(a);
+                double prob_b = distribution.CumulativeDistribution(b);
+                double intervalProbability = prob_b - prob_a;
+                ksi += (counts[i] * counts[i]) / (numOfExperiments * intervalProbability);
+
+            }
+            ksi -= numOfExperiments;
         }
         private void visualisation()
         {
-            for (int i = 0; i < 5; i++)
+            empMathExp = 0;
+            empMathVar = 0;
+            for (int i = 0; i < rundomVar.Length; i++)
             {
-                dataGridView3.Rows.Add(team[i, 0], team[i, 1], team[i, 2], team[i, 3], team[i, 4], team[i, 5], team[i, 6], team[i, 7]);
+                empMathExp = rundomVar.Average();
+                empMathVar = rundomVar.Variance();
             }
-        }
-        private void button2_Click(object sender, EventArgs e)
-        {
-            dataGridView3.Rows.Clear();
-            team = new int[numOfTeam, 8];
+            empMathExpError = Math.Abs(empMathExp - mathExpUser);
+            empMathVarError = Math.Abs(empMathVar - mathVarUser);
+            empMathExpError = empMathExpError / mathExpUser * 100;
+            empMathVarError = empMathVarError / mathVarUser * 100;
+
+            numericUpDown13.Value = (decimal)ksi;
+            Console.WriteLine(ksi);
+            label12.Text = (ksi < 24.996) ? "is true" : "is false";
+            for (int i = 0; i < k; i++)
+            {
+                float value = (float)counts[i] / (float)numOfExperiments;
+                chart1.Series[0].Points.AddXY(i + 1, value);
+            }
+
+            numericUpDown8.Value = (decimal)empMathExp;
+            numericUpDown10.Value = (decimal)Math.Round(empMathExpError, 3);
+            numericUpDown9.Value = (decimal)empMathVar;
+            numericUpDown11.Value = (decimal)Math.Round(empMathVarError, 3);
         }
     }
 }
