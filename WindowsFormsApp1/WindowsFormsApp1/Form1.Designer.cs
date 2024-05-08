158764e41888b16bb4c79e7c5dfcef9bbfbb1bef
Nikita Kalinichev <nikitakalinichev588@gmail.com>
2024-05-08 15:49:05 +0700

    up10lab

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -28,83 +28,23 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea6 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend6 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
-            this.label7 = new System.Windows.Forms.Label();
-            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.button1 = new System.Windows.Forms.Button();
-            this.numericUpDown7 = new System.Windows.Forms.NumericUpDown();
-            this.label8 = new System.Windows.Forms.Label();
-            this.numericUpDown8 = new System.Windows.Forms.NumericUpDown();
-            this.numericUpDown9 = new System.Windows.Forms.NumericUpDown();
-            this.label9 = new System.Windows.Forms.Label();
-            this.numericUpDown10 = new System.Windows.Forms.NumericUpDown();
-            this.label10 = new System.Windows.Forms.Label();
-            this.numericUpDown11 = new System.Windows.Forms.NumericUpDown();
-            this.label12 = new System.Windows.Forms.Label();
-            this.numericUpDown13 = new System.Windows.Forms.NumericUpDown();
-            this.label13 = new System.Windows.Forms.Label();
-            this.label14 = new System.Windows.Forms.Label();
-            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown7)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown8)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown9)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown10)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown11)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown13)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label_ = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
             this.SuspendLayout();
-            // 
-            // numericUpDown1
-            // 
-            this.numericUpDown1.DecimalPlaces = 2;
-            this.numericUpDown1.Location = new System.Drawing.Point(30, 92);
-            this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(120, 22);
-            this.numericUpDown1.TabIndex = 0;
-            this.numericUpDown1.Value = new decimal(new int[] {
-            12,
-            0,
-            0,
-            0});
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(27, 14);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(98, 16);
-            this.label7.TabIndex = 12;
-            this.label7.Text = "Число опытов";
-            // 
-            // chart1
-            // 
-            chartArea6.AxisY.Maximum = 1D;
-            chartArea6.AxisY.Minimum = 0D;
-            chartArea6.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea6);
-            legend6.Enabled = false;
-            legend6.Name = "Legend1";
-            this.chart1.Legends.Add(legend6);
-            this.chart1.Location = new System.Drawing.Point(174, -1);
-            this.chart1.Name = "chart1";
-            series6.ChartArea = "ChartArea1";
-            series6.Legend = "Legend1";
-            series6.Name = "Series1";
-            this.chart1.Series.Add(series6);
-            this.chart1.Size = new System.Drawing.Size(630, 456);
-            this.chart1.TabIndex = 14;
-            this.chart1.Text = "chart1";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(17, 395);
+            this.button1.Location = new System.Drawing.Point(30, 253);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(121, 35);
             this.button1.TabIndex = 15;
@@ -112,240 +52,119 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // numericUpDown7
+            // button2
             // 
-            this.numericUpDown7.Location = new System.Drawing.Point(30, 33);
-            this.numericUpDown7.Maximum = new decimal(new int[] {
-            10000,
-            0,
-            0,
-            0});
-            this.numericUpDown7.Name = "numericUpDown7";
-            this.numericUpDown7.Size = new System.Drawing.Size(120, 22);
-            this.numericUpDown7.TabIndex = 16;
-            this.numericUpDown7.Value = new decimal(new int[] {
-            100,
-            0,
-            0,
-            0});
+            this.button2.Location = new System.Drawing.Point(701, 253);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(117, 35);
+            this.button2.TabIndex = 17;
+            this.button2.Text = "Сбросить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // label8
+            // dataGridView3
             // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(829, 39);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(150, 16);
-            this.label8.TabIndex = 17;
-            this.label8.Text = "Выборочное среднее:";
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.label_,
+            this.Column7});
+            this.dataGridView3.Location = new System.Drawing.Point(30, 12);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.RowHeadersWidth = 51;
+            this.dataGridView3.RowTemplate.Height = 24;
+            this.dataGridView3.Size = new System.Drawing.Size(788, 235);
+            this.dataGridView3.TabIndex = 20;
             // 
-            // numericUpDown8
+            // Column1
             // 
-            this.numericUpDown8.DecimalPlaces = 2;
-            this.numericUpDown8.Increment = new decimal(new int[] {
-            2,
-            0,
-            0,
-            0});
-            this.numericUpDown8.Location = new System.Drawing.Point(832, 58);
-            this.numericUpDown8.Maximum = new decimal(new int[] {
-            1000,
-            0,
-            0,
-            0});
-            this.numericUpDown8.Minimum = new decimal(new int[] {
-            10,
-            0,
-            0,
-            -2147483648});
-            this.numericUpDown8.Name = "numericUpDown8";
-            this.numericUpDown8.Size = new System.Drawing.Size(142, 22);
-            this.numericUpDown8.TabIndex = 18;
+            this.Column1.HeaderText = "Команда";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 60;
             // 
-            // numericUpDown9
+            // Column2
             // 
-            this.numericUpDown9.DecimalPlaces = 2;
-            this.numericUpDown9.Location = new System.Drawing.Point(830, 215);
-            this.numericUpDown9.Name = "numericUpDown9";
-            this.numericUpDown9.Size = new System.Drawing.Size(144, 22);
-            this.numericUpDown9.TabIndex = 20;
+            this.Column2.HeaderText = "Игр";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 60;
             // 
-            // label9
+            // Column3
             // 
-            this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(827, 196);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(80, 16);
-            this.label9.TabIndex = 19;
-            this.label9.Text = "Дисперсия:";
+            this.Column3.HeaderText = "Выиграно";
+            this.Column3.MinimumWidth = 6;
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 80;
             // 
-            // numericUpDown10
+            // Column4
             // 
-            this.numericUpDown10.DecimalPlaces = 2;
-            this.numericUpDown10.Location = new System.Drawing.Point(832, 101);
-            this.numericUpDown10.Name = "numericUpDown10";
-            this.numericUpDown10.Size = new System.Drawing.Size(142, 22);
-            this.numericUpDown10.TabIndex = 22;
+            this.Column4.HeaderText = "Ничья";
+            this.Column4.MinimumWidth = 6;
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 60;
             // 
-            // label10
+            // Column5
             // 
-            this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(829, 83);
-            this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(96, 16);
-            this.label10.TabIndex = 21;
-            this.label10.Text = "Погрешность:";
+            this.Column5.HeaderText = "Проиграно";
+            this.Column5.MinimumWidth = 6;
+            this.Column5.Name = "Column5";
+            this.Column5.Width = 80;
             // 
-            // numericUpDown11
+            // Column6
             // 
-            this.numericUpDown11.DecimalPlaces = 2;
-            this.numericUpDown11.Location = new System.Drawing.Point(832, 259);
-            this.numericUpDown11.Name = "numericUpDown11";
-            this.numericUpDown11.Size = new System.Drawing.Size(144, 22);
-            this.numericUpDown11.TabIndex = 24;
+            this.Column6.HeaderText = "Очков";
+            this.Column6.MinimumWidth = 6;
+            this.Column6.Name = "Column6";
+            this.Column6.Width = 60;
             // 
-            // label12
+            // label_
             // 
-            this.label12.AutoSize = true;
-            this.label12.Location = new System.Drawing.Point(831, 384);
-            this.label12.Name = "label12";
-            this.label12.Size = new System.Drawing.Size(17, 16);
-            this.label12.TabIndex = 27;
-            this.label12.Text = "is";
+            this.label_.HeaderText = "Забито";
+            this.label_.MinimumWidth = 6;
+            this.label_.Name = "label_";
+            this.label_.Width = 60;
             // 
-            // numericUpDown13
+            // Column7
             // 
-            this.numericUpDown13.DecimalPlaces = 2;
-            this.numericUpDown13.Location = new System.Drawing.Point(832, 359);
-            this.numericUpDown13.Maximum = new decimal(new int[] {
-            1000,
-            0,
-            0,
-            0});
-            this.numericUpDown13.Name = "numericUpDown13";
-            this.numericUpDown13.Size = new System.Drawing.Size(144, 22);
-            this.numericUpDown13.TabIndex = 26;
-            // 
-            // label13
-            // 
-            this.label13.AutoSize = true;
-            this.label13.Location = new System.Drawing.Point(829, 340);
-            this.label13.Name = "label13";
-            this.label13.Size = new System.Drawing.Size(84, 16);
-            this.label13.TabIndex = 25;
-            this.label13.Text = "Хи-квадрат:";
-            // 
-            // label14
-            // 
-            this.label14.AutoSize = true;
-            this.label14.Location = new System.Drawing.Point(829, 240);
-            this.label14.Name = "label14";
-            this.label14.Size = new System.Drawing.Size(96, 16);
-            this.label14.TabIndex = 28;
-            this.label14.Text = "Погрешность:";
-            // 
-            // numericUpDown2
-            // 
-            this.numericUpDown2.DecimalPlaces = 2;
-            this.numericUpDown2.Increment = new decimal(new int[] {
-            2,
-            0,
-            0,
-            0});
-            this.numericUpDown2.Location = new System.Drawing.Point(30, 150);
-            this.numericUpDown2.Minimum = new decimal(new int[] {
-            100,
-            0,
-            0,
-            -2147483648});
-            this.numericUpDown2.Name = "numericUpDown2";
-            this.numericUpDown2.Size = new System.Drawing.Size(120, 22);
-            this.numericUpDown2.TabIndex = 29;
-            this.numericUpDown2.Value = new decimal(new int[] {
-            2,
-            0,
-            0,
-            0});
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(27, 73);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(77, 16);
-            this.label1.TabIndex = 30;
-            this.label1.Text = "Дисперсия";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(27, 131);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(104, 16);
-            this.label2.TabIndex = 31;
-            this.label2.Text = "Мат. ожидание";
+            this.Column7.HeaderText = "Пропущено";
+            this.Column7.MinimumWidth = 6;
+            this.Column7.Name = "Column7";
+            this.Column7.Width = 80;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(986, 450);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.numericUpDown2);
-            this.Controls.Add(this.label14);
-            this.Controls.Add(this.label12);
-            this.Controls.Add(this.numericUpDown13);
-            this.Controls.Add(this.label13);
-            this.Controls.Add(this.numericUpDown11);
-            this.Controls.Add(this.numericUpDown10);
-            this.Controls.Add(this.label10);
-            this.Controls.Add(this.numericUpDown9);
-            this.Controls.Add(this.label9);
-            this.Controls.Add(this.numericUpDown8);
-            this.Controls.Add(this.label8);
-            this.Controls.Add(this.numericUpDown7);
+            this.ClientSize = new System.Drawing.Size(857, 303);
+            this.Controls.Add(this.dataGridView3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.chart1);
-            this.Controls.Add(this.label7);
-            this.Controls.Add(this.numericUpDown1);
             this.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown7)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown8)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown9)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown10)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown11)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown13)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.NumericUpDown numericUpDown1;
-        private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.NumericUpDown numericUpDown7;
-        private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.NumericUpDown numericUpDown8;
-        private System.Windows.Forms.NumericUpDown numericUpDown9;
-        private System.Windows.Forms.Label label9;
-        private System.Windows.Forms.NumericUpDown numericUpDown10;
-        private System.Windows.Forms.Label label10;
-        private System.Windows.Forms.NumericUpDown numericUpDown11;
-        private System.Windows.Forms.Label label12;
-        private System.Windows.Forms.NumericUpDown numericUpDown13;
-        private System.Windows.Forms.Label label13;
-        private System.Windows.Forms.Label label14;
-        private System.Windows.Forms.NumericUpDown numericUpDown2;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn label_;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
     }
 }
 
--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -1,144 +1,93 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using MathNet.Numerics.Distributions;
-using MathNet.Numerics.Statistics;
-using static System.Windows.Forms.LinkLabel;
 
 namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
-        private int numOfExperiments;
-        private double[] rundomVar;
-        private double gap;
-        private int[] counts;
-        int k = 15;
-
-        private double generalProbability = 0;
+        private int numOfTeam = 6;
+        private int [,] team;
         private Random rnd = new Random();
-        double mathExp;
-        double mathVar;
-        double ksi;
-        double empMathVar;
-        double empMathExp;
-        double empMathVarError;
-        double empMathExpError;
-        double maxValue;
-        double minValue;
-
-        double mathExpUser;
-        double mathVarUser;
         public Form1()
         {
             InitializeComponent();
-            mathExp = 0;
-            mathVar = 0;
+            team = new int[numOfTeam, 8];
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            numOfExperiments = (int)numericUpDown7.Value;
-            rundomVar = new double[numOfExperiments];
-            mathVarUser = (double)numericUpDown1.Value;
-            mathExpUser = (double)numericUpDown2.Value;
-
-            counts = new int[k];
-            chart1.Series[0].Points.Clear();
-
-            listGenerate();
-            gridGenerate();
-            chi();
+            //numOfExperiments = (int)numericUpDown7.Value;
+            dataGridView3.Rows.Clear();
+            resultGenerate();
             visualisation();
         }
-        private void gridGenerate() {
-            maxValue = rundomVar.Max();
-            minValue = rundomVar.Min();
-            double sub = maxValue - minValue + 0.01;
-            gap = sub / k;
-            for (int i = 0; i < rundomVar.Length; i++)
-            {
-                int j = 0;
-                do
-                {
-                    j++;
-                } while (minValue + j * gap <= rundomVar[i]);
-                counts[j - 1]++;
-            }
-        }
-        private void listGenerate()
+        private void resultGenerate()
         {
-            for (int i = 0; i < numOfExperiments; i++)
+            for (int i = 0; i < numOfTeam; i++)
             {
-                int n = 12;
-                double rv = 0;
-                for (int j = 0; j < n; j++)
+                team[i, 0] = i + 1;
+                for (int j = i + 1; j < numOfTeam; j++)
                 {
-                    rv += rnd.NextDouble();
+                    if(i == j) { continue; }
+                    else
+                    {
+                        double currentRnd = rnd.NextDouble() * 10 / 2;
+                        int count_i = eval(currentRnd);
+                        int count_j = eval(currentRnd);
+                        team[i, 6] += count_i;
+                        team[i, 7] += count_j;
+                        team[j, 6] += count_j;
+                        team[j, 7] += count_i;
+                        if (count_i == count_j)
+                        {
+                            team[i, 3]++;
+                            team[j, 3]++;
+                            team[i, 5]++;
+                            team[j, 5]++;
+                        }
+                        else
+                        {
+                            if (count_i > count_j)
+                            {
+                                team[i, 2] ++;
+                                team[i, 5] += 3;
+                                team[j, 4] ++;
+                            }
+                            else
+                            {
+                                team[j, 2]++;
+                                team[j, 5] += 3;
+                                team[i, 4]++;
+                            }
+                        }
+                    }
                 }
-                rv = rv - 6;
-                double x = eval(rv);
-                x = mathExpUser + Math.Sqrt(mathVarUser) * x;
-                rundomVar[i] = x;
+                team[i, 1] = team[i, 3] + team[i, 4] + team[i, 2];
             }
         }
-        private double eval(double rv)
+        private int eval(double lambda)
         {
-            return rv + (1 / 240.0) * (Math.Pow(rv, 3) - 3 * rv);
-        }
-        private void chi()
-        {
-            var distribution = new Normal(mathExpUser, Math.Sqrt(mathVarUser));
-            ksi = 0;
-            for (int i = 0; i < k; i++)
+            int i = 0;
+            double S = 0;
+            do
             {
-                double a = minValue + i * gap;
-                double b = a + gap;
-
-                //функция плоности вер-ти 
-                double probabilityA = distribution.CumulativeDistribution(a);
-                double probabilityB = distribution.CumulativeDistribution(b);
-
-                // Вероятность интервала - разность между значениями PDF в верхнем и нижнем пределах интервала
-                double intervalProbability = probabilityB - probabilityA;
-                ksi += (counts[i] * counts[i]) / (numOfExperiments * intervalProbability);
-
-            }
-            ksi -= numOfExperiments;
+                double a = rnd.NextDouble();
+                S += Math.Log(a);
+                i++;
+            } while (S >= -lambda);
+            return i - 1;
         }
         private void visualisation()
         {
-            empMathExp = 0;
-            empMathVar = 0;
-            for (int i = 0; i < rundomVar.Length; i++)
+            for (int i = 0; i < 5; i++)
             {
-                empMathExp = rundomVar.Average();
-                empMathVar = rundomVar.Variance();
+                dataGridView3.Rows.Add(team[i, 0], team[i, 1], team[i, 2], team[i, 3], team[i, 4], team[i, 5], team[i, 6], team[i, 7]);
             }
-            empMathExpError = Math.Abs(empMathExp - mathExpUser);
-            empMathVarError = Math.Abs(empMathVar - mathVarUser);
-            empMathExpError = empMathExpError / mathExpUser * 100;
-            empMathVarError = empMathVarError / mathVarUser * 100;
-
-            numericUpDown13.Value = (decimal)ksi;
-            Console.WriteLine(ksi);
-            label12.Text = (ksi < 24.996) ? "is true" : "is false";
-            for (int i = 0; i < k; i++)
-            {
-                float value = (float)counts[i] / (float)numOfExperiments;
-                chart1.Series[0].Points.AddXY(i + 1, value);
-            }
-
-            numericUpDown8.Value = (decimal)empMathExp;
-            numericUpDown10.Value = (decimal)Math.Round(empMathExpError, 3);
-            numericUpDown9.Value = (decimal)empMathVar;
-            numericUpDown11.Value = (decimal)Math.Round(empMathVarError, 3);
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dataGridView3.Rows.Clear();
+            team = new int[numOfTeam, 8];
         }
     }
 }
